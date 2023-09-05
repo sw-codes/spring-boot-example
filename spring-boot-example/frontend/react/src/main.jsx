0a21a7329fe9ba713dd96175993596fe4069114e
sam <sam.twright@outlook.com>
2023-09-05 11:30:20 +0100

    Login and registration - react

--- spring-boot-example/frontend/react/src/main.jsx
+++ spring-boot-example/frontend/react/src/main.jsx
@@ -2,16 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {ChakraProvider, Toast} from '@chakra-ui/react'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createStandaloneToast} from '@chakra-ui/react'
+import Login from "./components/login/Login.jsx";
+import AuthProvider from "./components/context/AuthContext.jsx";
 import './index.css'
-import { createStandaloneToast } from '@chakra-ui/react'
+import ProtectedRoute from "./components/shared/ProtectedRoute.jsx";
+import Signup from "./components/signup/Signup.jsx";
 
-const { ToastContainer } = createStandaloneToast()
+const {ToastContainer} = createStandaloneToast()
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login/>
+    },
+    {
+        path: "/signup",
+        element: <Signup/>
+    },
+    {
+        path: "dashboard",
+        element: <ProtectedRoute><App/></ProtectedRoute>
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <ChakraProvider>
-          <App />
-          <ToastContainer />
-      </ChakraProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ChakraProvider>
+            <AuthProvider>
+                <RouterProvider router={router}/>
+            </AuthProvider>
+            <ToastContainer/>
+        </ChakraProvider>
+    </React.StrictMode>,
 )
